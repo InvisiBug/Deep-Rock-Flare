dc37acb9e6ff1e8042e0b3adfce1df9a61fc022a
InvisiBug <matthewjkavanagh@gmail.com>
2020-11-28 21:13:31 +0000

    Split out sensor functions

---
+++ src/X_Gryo_Sensor.ino
@@ -0,0 +1,109 @@
+void startSensor() {
+  Serial.println(F("Initializing I2C devices..."));
+  mpu.initialize();
+  pinMode(INTERRUPT_PIN, INPUT);
+
+  // verify connection
+  Serial.println(F("Testing device connections..."));
+  Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+
+  // wait for ready
+  // Serial.println(F("\nSend any character to begin DMP programming and demo: "));
+  // while (Serial.available() && Serial.read())
+  //   ;  // empty buffer
+  // while (!Serial.available())
+  //   ;  // wait for data
+  // while (Serial.available() && Serial.read())
+  //   ;  // empty buffer again
+
+  // load and configure the DMP
+  Serial.println(F("Initializing DMP..."));
+  devStatus = mpu.dmpInitialize();
+
+  // supply your own gyro offsets here, scaled for min sensitivity
+  mpu.setXGyroOffset(72);
+  mpu.setYGyroOffset(-51);
+  mpu.setZGyroOffset(28);
+  mpu.setXAccelOffset(-4254);
+  mpu.setYAccelOffset(-1984);
+  mpu.setZAccelOffset(1442);  // 1688 factory default for my test chip
+
+  // make sure it worked (returns 0 if so)
+  if (devStatus == 0) {
+    // Calibration Time: generate offsets and calibrate our MPU6050
+    mpu.CalibrateAccel(6);
+    mpu.CalibrateGyro(6);
+    mpu.PrintActiveOffsets();
+    // turn on the DMP, now that it's ready
+    Serial.println(F("Enabling DMP..."));
+    mpu.setDMPEnabled(true);
+
+    // enable Arduino interrupt detection
+    Serial.print(F("Enabling interrupt detection (Arduino external interrupt "));
+    Serial.print(digitalPinToInterrupt(INTERRUPT_PIN));
+    Serial.println(F(")..."));
+    attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
+    mpuIntStatus = mpu.getIntStatus();
+
+    // set our DMP Ready flag so the main loop() function knows it's okay to use it
+    Serial.println(F("DMP ready! Waiting for first interrupt..."));
+    dmpReady = true;
+
+    // get expected DMP packet size for later comparison
+    packetSize = mpu.dmpGetFIFOPacketSize();
+  } else {
+    // ERROR!
+    // 1 = initial memory load failed
+    // 2 = DMP configuration updates failed
+    // (if it's going to break, usually the code will be 1)
+    Serial.print(F("DMP Initialization failed (code "));
+    Serial.print(devStatus);
+    Serial.println(F(")"));
+  }
+}
+
+void realWorldAccel() {
+  // display initial world-frame acceleration, adjusted to remove gravity
+  // and rotated based on known orientation from quaternion
+  mpu.dmpGetQuaternion(&q, fifoBuffer);
+  mpu.dmpGetAccel(&aa, fifoBuffer);
+  mpu.dmpGetGravity(&gravity, &q);
+  mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
+  mpu.dmpGetLinearAccelInWorld(&aaWorld, &aaReal, &q);
+  Serial.print("aworld\t");
+  Serial.print(aaWorld.x);
+  Serial.print("\t");
+  Serial.print(aaWorld.y);
+  Serial.print("\t");
+  Serial.println(aaWorld.z);
+}
+
+void realAccel() {
+  mpu.dmpGetQuaternion(&q, fifoBuffer);
+  mpu.dmpGetAccel(&aa, fifoBuffer);
+  mpu.dmpGetGravity(&gravity, &q);
+  mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
+  Serial.print("areal\t");
+  Serial.print(aaReal.x);
+  Serial.print("\t");
+  Serial.print(aaReal.y);
+  Serial.print("\t");
+  Serial.println(aaReal.z);
+}
+
+void yawPitchRoll() {
+  // display Euler angles in degrees
+  mpu.dmpGetQuaternion(&q, fifoBuffer);
+  mpu.dmpGetGravity(&gravity, &q);
+  mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+  Serial.print("ypr\t");
+  Serial.print(ypr[0] * 180 / M_PI);
+  Serial.print("\t");
+  Serial.print(ypr[1] * 180 / M_PI);
+  Serial.print("\t");
+  Serial.println(ypr[2] * 180 / M_PI);
+}
+
+void dmpDataReady() {
+  mpuInterrupt = true;
+}
--- src/main.ino
+++ src/main.ino
@@ -1,15 +1,56 @@
-
+////////////////////////////////////////////////////////////////////////
+//
+//  ###
+//   #  #    #  ####  #      #    # #####  ######  ####
+//   #  ##   # #    # #      #    # #    # #      #
+//   #  # #  # #      #      #    # #    # #####   ####
+//   #  #  # # #      #      #    # #    # #           #
+//   #  #   ## #    # #      #    # #    # #      #    #
+//  ### #    #  ####  ######  ####  #####  ######  ####
+//
+////////////////////////////////////////////////////////////////////////
 #include "I2Cdev.h"
 #include "MPU6050_6Axis_MotionApps20.h"
-
 // #include "Wire.h" // * Dont think this is needed
-
-MPU6050 mpu;
-
-#define OUTPUT_READABLE_YAWPITCHROLL
-
+////////////////////////////////////////////////////////
+////////////////
+//
+//  ######
+//  #     # ###### ###### # #    # # ##### #  ####  #    #  ####
+//  #     # #      #      # ##   # #   #   # #    # ##   # #
+//  #     # #####  #####  # # #  # #   #   # #    # # #  #  ####
+//  #     # #      #      # #  # # #   #   # #    # #  # #      #
+//  #     # #      #      # #   ## #   #   # #    # #   ## #    #
+//  ######  ###### #      # #    # #   #   #  ####  #    #  ####
+//
+////////////////////////////////////////////////////////////////////////
 #define INTERRUPT_PIN 2  // use pin 2 on Arduino Uno & most boards
 #define LED_PIN 13       // (Arduino is 13, Teensy is 11, Teensy++ is 6)
+
+////////////////////////////////////////////////////////////////////////
+//
+//  #     #
+//  #     #   ##   #####  #####  #    #   ##   #####  ######
+//  #     #  #  #  #    # #    # #    #  #  #  #    # #
+//  ####### #    # #    # #    # #    # #    # #    # #####
+//  #     # ###### #####  #    # # ## # ###### #####  #
+//  #     # #    # #   #  #    # ##  ## #    # #   #  #
+//  #     # #    # #    # #####  #    # #    # #    # ######
+//
+////////////////////////////////////////////////////////////////////////
+MPU6050 mpu;
+
+////////////////////////////////////////////////////////////////////////
+//
+//  #     #
+//  #     #   ##   #####  #   ##   #####  #      ######  ####
+//  #     #  #  #  #    # #  #  #  #    # #      #      #
+//  #     # #    # #    # # #    # #####  #      #####   ####
+//   #   #  ###### #####  # ###### #    # #      #           #
+//    # #   #    # #   #  # #    # #    # #      #      #    #
+//     #    #    # #    # # #    # #####  ###### ######  ####
+//
+////////////////////////////////////////////////////////////////////////
 bool blinkState = false;
 
 // MPU control/status vars
@@ -29,18 +70,19 @@ VectorFloat gravity;  // [x, y, z]            gravity vector
 float euler[3];       // [psi, theta, phi]    Euler angle container
 float ypr[3];         // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
 
-// ================================================================
-// ===               INTERRUPT DETECTION ROUTINE                ===
-// ================================================================
 volatile bool mpuInterrupt = false;  // indicates whether MPU interrupt pin has gone high
-void dmpDataReady() {
-  mpuInterrupt = true;
-}
 
-// ================================================================
-// ===                      INITIAL SETUP                       ===
-// ================================================================
-
+////////////////////////////////////////////////////////////////////////
+//
+//  ######                                                #####
+//  #     # #####   ####   ####  #####    ##   #    #    #     # #####   ##   #####  ##### #    # #####
+//  #     # #    # #    # #    # #    #  #  #  ##  ##    #         #    #  #  #    #   #   #    # #    #
+//  ######  #    # #    # #      #    # #    # # ## #     #####    #   #    # #    #   #   #    # #    #
+//  #       #####  #    # #  ### #####  ###### #    #          #   #   ###### #####    #   #    # #####
+//  #       #   #  #    # #    # #   #  #    # #    #    #     #   #   #    # #   #    #   #    # #
+//  #       #    #  ####   ####  #    # #    # #    #     #####    #   #    # #    #   #    ####  #
+//
+////////////////////////////////////////////////////////////////////////
 void setup() {
   // join I2C bus (I2Cdev library doesn't do this automatically)
 
@@ -48,86 +90,24 @@ void setup() {
   // (115200 chosen because it is required for Teapot Demo output, but it's
   // really up to you depending on your project)
   Serial.begin(115200);
-  while (!Serial)
-    ;  // wait for Leonardo enumeration, others continue immediately
 
-  // NOTE: 8MHz or slower host processors, like the Teensy @ 3.3V or Arduino
-  // Pro Mini running at 3.3V, cannot handle this baud rate reliably due to
-  // the baud timing being too misaligned with processor ticks. You must use
-  // 38400 or slower in these cases, or use some kind of external separate
-  // crystal solution for the UART timer.
-
-  // initialize device
-  Serial.println(F("Initializing I2C devices..."));
-  mpu.initialize();
-  pinMode(INTERRUPT_PIN, INPUT);
-
-  // verify connection
-  Serial.println(F("Testing device connections..."));
-  Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
-
-  // wait for ready
-  Serial.println(F("\nSend any character to begin DMP programming and demo: "));
-  while (Serial.available() && Serial.read())
-    ;  // empty buffer
-  while (!Serial.available())
-    ;  // wait for data
-  while (Serial.available() && Serial.read())
-    ;  // empty buffer again
-
-  // load and configure the DMP
-  Serial.println(F("Initializing DMP..."));
-  devStatus = mpu.dmpInitialize();
-
-  // supply your own gyro offsets here, scaled for min sensitivity
-  mpu.setXGyroOffset(72);
-  mpu.setYGyroOffset(-51);
-  mpu.setZGyroOffset(28);
-  mpu.setXAccelOffset(-4254);
-  mpu.setYAccelOffset(-1984);
-  mpu.setZAccelOffset(1442);  // 1688 factory default for my test chip
-
-  // make sure it worked (returns 0 if so)
-  if (devStatus == 0) {
-    // Calibration Time: generate offsets and calibrate our MPU6050
-    mpu.CalibrateAccel(6);
-    mpu.CalibrateGyro(6);
-    mpu.PrintActiveOffsets();
-    // turn on the DMP, now that it's ready
-    Serial.println(F("Enabling DMP..."));
-    mpu.setDMPEnabled(true);
-
-    // enable Arduino interrupt detection
-    Serial.print(F("Enabling interrupt detection (Arduino external interrupt "));
-    Serial.print(digitalPinToInterrupt(INTERRUPT_PIN));
-    Serial.println(F(")..."));
-    attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
-    mpuIntStatus = mpu.getIntStatus();
-
-    // set our DMP Ready flag so the main loop() function knows it's okay to use it
-    Serial.println(F("DMP ready! Waiting for first interrupt..."));
-    dmpReady = true;
-
-    // get expected DMP packet size for later comparison
-    packetSize = mpu.dmpGetFIFOPacketSize();
-  } else {
-    // ERROR!
-    // 1 = initial memory load failed
-    // 2 = DMP configuration updates failed
-    // (if it's going to break, usually the code will be 1)
-    Serial.print(F("DMP Initialization failed (code "));
-    Serial.print(devStatus);
-    Serial.println(F(")"));
-  }
+  startSensor();
 
   // configure LED for output
   pinMode(LED_PIN, OUTPUT);
 }
 
-// ================================================================
-// ===                    MAIN PROGRAM LOOP                     ===
-// ================================================================
-
+///////////////////////////////////////////////////////////////////////
+//
+//  #     #                    ######
+//  ##   ##   ##   # #    #    #     # #####   ####   ####  #####    ##   #    #
+//  # # # #  #  #  # ##   #    #     # #    # #    # #    # #    #  #  #  ##  ##
+//  #  #  # #    # # # #  #    ######  #    # #    # #      #    # #    # # ## #
+//  #     # ###### # #  # #    #       #####  #    # #  ### #####  ###### #    #
+//  #     # #    # # #   ##    #       #   #  #    # #    # #   #  #    # #    #
+//  #     # #    # # #    #    #       #    #  ####   ####  #    # #    # #    #
+//
+///////////////////////////////////////////////////////////////////////
 void loop() {
   // if programming failed, don't try to do anything
   if (!dmpReady) return;
